9a4772a340ee056c8c1b1af0fcb77b3ee19d4711
harshrjjpt <harshrjjpt@gmail.com>
2023-05-30 14:14:16 +0530

    initial template files

---
+++ network_substream/src/lib.rs
@@ -0,0 +1,17 @@
+mod pb;
+
+use pb::block_meta::BlockMeta;
+use substreams::Hex;
+use substreams_ethereum::pb::eth;
+
+#[substreams::handlers::map]
+fn map_block(block: eth::v2::Block) -> Result<BlockMeta, substreams::errors::Error> {
+    let header = block.header.as_ref().unwrap();
+
+    Ok(BlockMeta {
+        number: block.number,
+        hash: Hex(&block.hash).to_string(),
+        parent_hash: Hex(&header.parent_hash).to_string(),
+        timestamp: header.timestamp.as_ref().unwrap().to_string(),
+    })
+}
---
+++ network_substream/src/pb/mod.rs
@@ -0,0 +1,3 @@
+#[path = "sf.ethereum.block_meta.v1.rs"]
+#[allow(dead_code)]
+pub mod block_meta;

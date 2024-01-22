4cdd8cfe026780e433f03240ace032bfd74bc56c
Vikash Choubey <5386997+realchoubey@users.noreply.github.com>
2024-01-22 16:28:08 +0530

    Code restructure  (#16)
    * Updated naming convention of folder for better understanding
    
    * Warning message correction
    
    * Warning message correction
    
    * Clickhouse support changes
    
    * Clickhouse support DSN:

---
+++ db_sink_substream/schemas/network_schema/clean.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS block_details;
+DROP TABLE IF EXISTS cursors;
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS blocks;
--- substreams-sink/schemas/eth-block-meta/clean.sql
+++
@@ -1,4 +0,0 @@
-drop table if exists block_details;
-drop table if exists cursors;
-drop table if exists transactions;
-drop table if exists blocks;

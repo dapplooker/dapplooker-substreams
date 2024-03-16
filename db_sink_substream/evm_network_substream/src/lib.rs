14695ce44d9ec24a1b629c20d7404e48b8f76340
Vikash Choubey <choubey@Vikashs-MacBook-Pro.local>
2024-03-16 10:48:51 +0530

    Code formatting and manifest file support

--- db_sink_substream/evm_network_substream/src/lib.rs
+++ db_sink_substream/evm_network_substream/src/lib.rs
@@ -10,31 +10,22 @@ use substreams_database_change::pb::database::{DatabaseChanges};
 use substreams_ethereum::pb::eth::v2::BigInt;
 
 
-// create block entity
+// Create block entity
 fn add_block_entity(tables: &mut Tables, blk: &eth::Block) {
     let header = blk.header.as_ref().unwrap();
-
     tables
-        .create_row("blocks", blk.number.to_string())
-        .set("hash", base_64_to_hex(blk.hash.clone()))
-        .set(
-            "parent_hash",
-            Hex(&blk.header.as_ref().unwrap().parent_hash).to_string(),
-        )
-        .set(
-            "timestamp",
-            blk.header.as_ref().unwrap().timestamp.as_ref().unwrap(),
-        )
-        .set("size", blk.size)
-        .set("nonce", header.nonce)
-        .set("gas_limit", header.gas_limit)
-        .set("gas_used", header.gas_used);
+	.create_row("blocks", blk.number.to_string())
+	.set("hash", base_64_to_hex(blk.hash.clone()))
+	.set("parent_hash", Hex(&blk.header.as_ref().unwrap().parent_hash).to_string())
+	.set("timestamp",blk.header.as_ref().unwrap().timestamp.as_ref().unwrap())
+	.set("size", blk.size)
+	.set("nonce", header.nonce)
+	.set("gas_limit", header.gas_limit)
+	.set("gas_used", header.gas_used);
 }
 
-//create transaction entity
-fn add_trx_info_entity(tables: &mut Tables, trx: &eth::TransactionTrace,  block_number: &u64,
-    time_stamp: &Timestamp) {
-
+// Create transaction entity
+fn add_trx_info_entity(tables: &mut Tables, trx: &eth::TransactionTrace,  block_number: &u64, time_stamp: &Timestamp) {
     tables
     .create_row("transactions",  base_64_to_hex(trx.hash.clone()))
     .set("status", trx.status)
@@ -51,24 +42,22 @@ fn add_trx_info_entity(tables: &mut Tables, trx: &eth::TransactionTrace,  block_
     .set("amount",  option_bigint_to_number_u64(trx.value.clone()));
 }
 
-//create contract entity
-fn add_contracts_info_entity(tables: &mut Tables, trx: &eth::TransactionTrace,  block_number: &u64,
-    time_stamp: &Timestamp) {
-
+// Create contract entity
+fn add_contracts_info_entity(tables: &mut Tables, trx: &eth::TransactionTrace,  block_number: &u64, time_stamp: &Timestamp) {
     tables
-        .create_row("contracts",  base_64_to_hex(trx.to.clone()))
-        .set("id", base_64_to_hex(trx.hash.clone()))
-        .set("transaction_hash", base_64_to_hex(trx.hash.clone()))
-        .set("owner",  base_64_to_hex(trx.from.clone()))
-        .set("block_number",  block_number.clone())
-        .set("timestamp",  time_stamp);
+	.create_row("contracts",  base_64_to_hex(trx.to.clone()))
+	.set("id", base_64_to_hex(trx.hash.clone()))
+	.set("transaction_hash", base_64_to_hex(trx.hash.clone()))
+	.set("owner",  base_64_to_hex(trx.from.clone()))
+	.set("block_number",  block_number.clone())
+	.set("timestamp",  time_stamp);
 }
 
-//base64 to hex
+// base64 to hex
 fn base_64_to_hex<T: std::convert::AsRef<[u8]>>(num:T) -> String {
     let num = hex::encode(&num);
     let num = num.to_string();
-     format!("0x{}", &num)
+    format!("0x{}", &num);
 }
 
 // bigint to string. Commented as not using currently
@@ -104,24 +93,23 @@ fn option_bigint_to_number_u64(bigint: Option<BigInt>) -> u64 {
 
  // main db_out function
 #[substreams::handlers::map]
-fn db_out(
-    blk: eth::Block
-) -> Result<DatabaseChanges, substreams::errors::Error> {
+fn db_out(blk: eth::Block) -> Result<DatabaseChanges, substreams::errors::Error> {
     let header = &blk.header.as_ref().unwrap();
     let block_number = &blk.number;
     let time_stamp =  header.timestamp.as_ref().unwrap();
     let mut tables = Tables::new();
+
     // get blocks data
     add_block_entity(&mut tables, &blk);
     for trx in &blk.transaction_traces{
         // get transactions data
         if trx.status == TransactionTraceStatus::Succeeded as i32 {
             add_trx_info_entity(&mut tables, &trx, block_number, time_stamp);
-        let contract_check = String::from_utf8_lossy(&trx.input).to_string();
-         if contract_check.starts_with("`�`@R") && base_64_to_hex(trx.to.clone()) != "0x0000000000000000000000000000000000000000"  {
-            //get contracts data
-            add_contracts_info_entity(&mut tables, &trx, block_number, time_stamp);
-        }
+            let contract_check = String::from_utf8_lossy(&trx.input).to_string();
+            // Check for contract creation and store the data
+            if contract_check.starts_with("`�`@R") && base_64_to_hex(trx.to.clone()) != "0x0000000000000000000000000000000000000000"  {
+            	add_contracts_info_entity(&mut tables, &trx, block_number, time_stamp);
+			}
         }
     }
     Ok(tables.to_database_changes())
